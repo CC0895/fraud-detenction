560f9e940eeb476adc1c0a5a8bd62f42ccd161a0
CC0895 <claudio.camele@gmail.com>
2025-03-25 11:18:17 +0100

    connection to transformation db

---
+++ silver_level/main.py
@@ -0,0 +1,8 @@
+import os
+
+from persistence.login_db_service import DatabaseConnector
+
+if __name__ == "__main__":
+    db_connector = DatabaseConnector()
+    connection = db_connector.connect()
+    db_connector.close_connection()
---
+++ silver_level/persistence/login_db_service.py
@@ -0,0 +1,42 @@
+import os
+import logging
+import psycopg2
+from psycopg2.extras import LoggingConnection
+
+class DatabaseConnector:
+    def __init__(self):
+        self.logger = self._setup_logger()
+        self.conn = None
+
+    def _setup_logger(self):
+        logging.basicConfig(level=logging.DEBUG)
+        logger = logging.getLogger(self.__class__.__name__)
+        return logger
+
+    def connect(self):
+        db_settings = {
+            "user": os.getenv("POSTGRES_USER"),
+            "password": os.getenv("POSTGRES_PASSWORD"),
+            "host": "fraud_analysis_transformation",
+            "database": os.getenv("POSTGRES_DB"),
+        }
+
+        try:
+            self.conn = psycopg2.connect(connection_factory=LoggingConnection, **db_settings)
+            self.conn.initialize(self.logger)
+            self.logger.info("Connessione al database riuscita!")
+            return self.conn
+
+        except Exception as e:
+            self.logger.error(f"Errore durante la connessione al database: {e}")
+            return None
+
+    def close_connection(self):
+        if self.conn:
+            try:
+                self.conn.close()
+                self.logger.info("Connessione al database chiusa con successo.")
+            except Exception as e:
+                self.logger.error(f"Errore durante la chiusura della connessione: {e}")
+
+
